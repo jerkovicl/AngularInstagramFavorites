602a14b929228f8fd1306e05d1ea5825b4f00120
Luka Jerkovic <luka.jerkovic@sistemi.hr>
2014-07-29 11:39:38 +0200

    base setup done

--- js/custom/ui-instagram.js
+++ js/custom/ui-instagram.js
@@ -1,32 +1,43 @@
-(function () {
-    angular.module('ui.instagram', []).service('Instagram', function ($http) {
-        return {
-            get: function (id, clientId, cb) {
-                var URL;
-                URL = "https://api.instagram.com/v1/users/" + id + "/media/recent/?client_id=" + clientId + "&callback=JSON_CALLBACK";
-                $http.jsonp(URL).success(function (resolution) {
-                    cb(resolution.data);
-                });
-            }
-        };
-    }).directive('uiInstagram', function (Instagram) {
-        return {
-            restrict: 'EA',
-            scope: {
-                userId: '=',
-                clientId: '='
-            },
-            templateUrl: 'js/custom/instagramTpl.html',
-            link: function (scope, element, attrs) {
-                scope.grams = [];
-                Instagram.get(scope.userId, scope.clientId, function (grams) {
-                    scope.grams = grams;
-                });
-            }
-        };
-    });
+var app =
+    angular.module('ui.instagram', []);
 
-}).call(this);
+app.service('Instagram', function ($http) {
+    return {
+        get: function (id, clientId, cb) {
+            var URL;
+            URL = "https://api.instagram.com/v1/users/" + id + "/media/recent/?client_id=" + clientId + "&callback=JSON_CALLBACK";
+            $http.jsonp(URL).success(function (resolution) {
+                cb(resolution.data);
+            });
+        }
+    };
+});
+app.directive('uiInstagram', function (Instagram) {
+    return {
+        restrict: 'EA',
+        scope: {
+            userId: '=',
+            clientId: '='
 
-//8263704
-//19719473
+        },
+        template: '<img ng-repeat="gram in grams" ng-src="{{ gram.images.standard_resolution.url }}"></img>',
+        link: function (scope, element, attrs) {
+            console.log(scope);
+            scope.grams = [];
+            Instagram.get(scope.userId, scope.clientId, function (grams) {
+                console.log(grams);
+                scope.grams = grams;
+            });
+        }
+    };
+});
+
+app.controller("demoCtrl", function ($scope) {
+    $scope.users = [{
+        id: 19719473,
+        clientId: "8a607f8a109b4e04a831fe0309ceff0b"
+    }, {
+        id: 8263704,
+        clientId: "8a607f8a109b4e04a831fe0309ceff0b"
+    }];
+});
